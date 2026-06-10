b1560ed0a7fe091940776f88abb17f789540f1f7
flicker1016 <xcvy2141758@gmail.com>
2026-06-10 13:28:27 +0900

    update commit

--- src/blackjack/V01.java
+++ src/blackjack/V01.java
@@ -1,10 +1,27 @@
 package blackjack;
 
-public class version01 {
+import java.util.Collections;
+
+public class V01 {
 
 	public static void main(String[] args) {
 
+		 // 카드 랜덤 생성
+		// 랜덤, 셔플
+		// 배열 or 스트림
 		
+		
+		// Scanner
+		// Hit, Stand 
+		
+		// 조건 : 21이 되면  그리고 21 초과 
+		// while if break
+		
+		// 최종 합 sum 
+		
+		
+				
+		 Collections.shuffle(null, null);
 		
 		
 		
